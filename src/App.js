f6bd8945a712cd95c367034647104755fc6ec644
ceciboscarino <ceciboscarino@hotmail.com>
2023-02-17 18:48:20 -0300

    Contador terminado

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@
 import './App.css';
 import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import Boton from './componentes/Boton'
+import Contador from  './componentes/Contador'
+import React from 'react';
+import {useState} from 'react';
+
+
 
 function App() {
+  const[ numClicks, setNumClicks ] = useState (0);
 
-  function sumar (){ }
-  function reiniciar (){ }
+  const manejarClick = () => {
+    setNumClicks (numClicks + 1); 
+  }
+  const reiniciarontador = () => {
+    setNumClicks (0);
+  }
+
   return (
     <div className="App">
       <div className='freecodecamp-logo-contenedor'>
@@ -15,15 +26,16 @@ function App() {
         alt='Logo de freeCodeCamp'/>
       </div>
       <div className='contenedor-principal'>
+        <Contador numClicks= {numClicks}/>
         <Boton
         texto= "Click"
         esBotonDeClick= {true}
-        manejarClick= {sumar}
+        manejarClick= {manejarClick}
         />
         <Boton
         texto= "Reiniciar"
         esBotonDeClick= {false}
-        manejarClick= {reiniciar}
+        manejarClick= {reiniciarontador}
         />
       </div>
     </div>
